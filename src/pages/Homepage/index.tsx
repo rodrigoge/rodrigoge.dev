b0717de70eaa249f20f66e9f43e34335efc5d977
Rodrigo Gouveia <rgestevao@gmail.com>
2023-05-04 21:12:50 -0300

    feat: inserting hero section in homepage

--- src/pages/Homepage/index.tsx
+++ src/pages/Homepage/index.tsx
@@ -1,9 +1,27 @@
 import './styles.scss';
+import heroImg from '../../assets/hero-image.svg';
 import BackgroundPattern from '../../components/BackgroundPattern';
 
 export default function Homepage() {
     return (
         <div className='dark'>
+            <div className="homepage-container">
+                <form>
+                    <div className="hero-section">
+                        <h1>Hi! I am Rodrigo Gouveia</h1>
+                        <div className="tags">
+                            <span>Frontend</span>
+                            <span>Microservices</span>
+                        </div>
+                        <h2>Fullstack Developer at HST Card Technology</h2>
+                        <span>Developing applications and systems for 3 years as a fullstack developer.</span>
+                    </div>
+
+                    <aside>
+                        <img src={heroImg} alt="Hero image" />
+                    </aside>
+                </form>
+            </div>
             <BackgroundPattern />
         </div>
     );
