90db50b9ef619b26c3329e3813083400f0a0e351
Rodrigo Gouveia <rgestevao@gmail.com>
2023-06-03 16:15:30 -0300

    bugfix: fixing page name

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomePage from '../pages/HomePage';
+import HomePage from "../pages/HomePage";
 import ProjectPage from "../pages/ProjectPage";
 import AboutPage from "../pages/AboutPage";
 
